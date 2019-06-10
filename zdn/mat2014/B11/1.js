a07c78669acdb5e79b9e16abe400041966e96b86
Aisse-258 <y-kate@ukr.net>
2019-06-10 11:17:50 +0300

    Доработка "Рефакторинг zdn/mat2014/B11/1 #646"

--- zdn/mat2014/B11/1.js
+++ zdn/mat2014/B11/1.js
@@ -1,20 +1,16 @@
 (function() {
-
-	var a = sluchch(2,99),
+	'use strict';
+	var a = [sluchch(2,99),sluchch(-99,-2)].iz(),
 		b = sluchch(1,89),
 		c = sluchch(1,2),
-		d = sluchch(1,99),
+		d = [sluchch(1,99),sluchch(-99,-1)].iz(),
 		f = (c%2)?(-1):(1),
 		g = ['\\cos','\\sin'].shuffle();
 
 	chas2.task.setTask({
 		text: ('Найдите значение выражения $$\\frac{'+a+g[0]+' '+b+'^\\circ}{'+g[1]+' '+(90+c*180-b)+'^\\circ}+'+d+'$$').plusminus(),
-		answers: [''+(a*f+d),
-			],
+		answers: ''+(a*f+d),
 		tags: {
-			'log': 0,
-			'prz': 0,
-			'drs': 0,
 			'tri': 1,
 		},
 	});
